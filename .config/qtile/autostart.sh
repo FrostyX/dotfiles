998fd6b59c7e084bcd087d1d6c9a23c8ece942f2
Jakub Kadlcik <frostyx@email.cz>
2019-08-03 00:09:36 +0200

    Turn off screensaver

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -4,5 +4,10 @@ xsetroot -cursor_name left_ptr
 
 feh --bg-scale /home/jkadlcik/.config/qtile/img/kde-wallpaper.png
 
+# Turn off black Screensaver
+# My current laptop has an issue that sometimes causes system to freeze
+# on the black screen, requiring a hard reboot to fix.
+xset s off -dpms
+
 gnome-screensaver &
 nm-applet &
