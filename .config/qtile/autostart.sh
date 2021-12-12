5dec5c3d2a151708f85f49c573668d49fd07e72c
Jakub Kadlcik <frostyx@email.cz>
2021-12-12 15:58:48 +0100

    qtile: disable system bell

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -9,6 +9,10 @@ feh --bg-scale ~/.config/qtile/img/paint-colorful-overlay.jpg
 # on the black screen, requiring a hard reboot to fix.
 xset s off -dpms
 
+# Turn off system beep in console:
+xset b off
+xset b 0 0 0
+
 gnome-screensaver &
 nm-applet &
 blueman-applet &
