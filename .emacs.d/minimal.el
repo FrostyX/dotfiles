7f93c53f6594ff1c58c87f48812d2483a306b019
Jakub Kadlcik <frostyx@email.cz>
2022-05-06 10:22:51 -0400

    emacs: add minimal config

---
+++ .emacs.d/minimal.el
@@ -0,0 +1,32 @@
+(require 'package)
+(setq package-archives
+   '(("melpa" . "https://melpa.org/packages/")
+     ("melpa-stable" . "https://stable.melpa.org/packages/")
+     ("gnu" . "http://elpa.gnu.org/packages/")
+     ("nongnu" . "https://elpa.nongnu.org/nongnu/")))
+
+(setq package-enable-at-startup nil)
+(package-initialize)
+
+;; bootstrap use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+
+
+(setq evil-want-keybinding nil)
+(setq evil-want-integration t)
+
+(use-package evil
+  :ensure t
+  :init
+  (setq evil-search-module 'evil-search)
+  (setq evil-ex-complete-emacs-commands nil)
+  (setq evil-vsplit-window-right t)
+  (setq evil-split-window-below t)
+  (setq evil-shift-round nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-ex-set-initial-state 'normal)
+  :config
+  (evil-mode))
