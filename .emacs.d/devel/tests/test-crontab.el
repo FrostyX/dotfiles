653383720d4a834b1024ffe9fa9fc859cfd0d1bf
Jakub Kadlcik <frostyx@email.cz>
2023-11-01 15:15:34 +0100

    emacs: add write some crontab tests

--- .emacs.d/devel/tests/test-crontab.el
+++ .emacs.d/devel/tests/test-crontab.el
@@ -12,11 +12,41 @@
   (load-file path))
 
 
-(ert-deftest test-foo ()
-  (should (equal (+ 1 2) 4)))
+(ert-deftest test-human-friendly-simple ()
+  (should (equal (crontab-human-friendly "5 4 * * 7")
+                 "At 04:05 on Sunday."))
 
-(ert-deftest test-bar ()
-  (should nil))
+  (should (equal (crontab-human-friendly "15 14 1 * *")
+                 "At 14:15 on day-of-month 1."))
 
-(ert-deftest test-human-friendly ()
-  (should (equal (crontab-human-friendly) "At 04:05 on Sunday.")))
+  (should (equal (crontab-human-friendly "5 0 * 8 *")
+                 "At 00:05 in August."))
+
+  (should (equal (crontab-human-friendly "15 14 1 * *")
+                 "At 14:15 on day-of-month 1.")))
+
+
+(ert-deftest test-human-friendly-advanced ()
+  (should (equal (crontab-human-friendly "0 22 * * 1-5")
+                 "At 22:00 on every day-of-week from Monday through Friday."))
+
+  (should (equal (crontab-human-friendly "23 0-20/2 * * *")
+                 "At minute 23 past every 2nd hour from 0 through 20."))
+
+  (should (equal (crontab-human-friendly "0 0,12 1 */2 *")
+                 "At minute 0 past hour 0 and 12 on day-of-month 1 in every 2nd month."))
+
+  (should (equal (crontab-human-friendly "0 4 8-14 * *")
+                 "At 04:00 on every day-of-month from 8 through 14."))
+
+  (should (equal (crontab-human-friendly "0 0 1,15 * 3")
+                 "At 00:00 on day-of-month 1 and 15 and on Wednesday."))
+
+  (should (equal (crontab-human-friendly "@weekly")
+                 "At 00:00 on Sunday."))
+
+  (should (equal (crontab-human-friendly "0 22 * * 1-5")
+                 "At 22:00 on every day-of-week from Monday through Friday."))
+
+  (should (equal (crontab-human-friendly "23 0-20/2 * * *")
+                 "At minute 23 past every 2nd hour from 0 through 20.")))
