8a90e24b2f5b124b17e2acc4a365ffb71d46fa95
Jakub Kadlcik <frostyx@email.cz>
2025-07-13 23:20:41 +0200

    zen: initial configuration

---
+++ .zen/Work/chrome/README.md
@@ -0,0 +1,10 @@
+See the instructions here
+https://github.com/catppuccin/zen-browser
+
+Currently using Latte > Sky
+
+I removed these settings from the original:
+
+```css
+--toolbarbutton-icon-fill: #04a5e5 !important;
+```
---
+++ .zen/Work/user.js
@@ -0,0 +1,12 @@
+// We can copy-paste settings from prefs.js
+
+user_pref("browser.startup.homepage", "https://google.com");
+user_pref("general.smoothScroll.msdPhysics.enabled", false);
+
+user_pref("zen.theme.content-element-separation", 0);
+user_pref("zen.glance.enabled", false);
+user_pref("zen.view.show-newtab-button-top", false);
+user_pref("zen.view.use-single-toolbar", false);
+user_pref("zen.welcome-screen.seen", true);
+
+user_pref("browser.uiCustomization.state", "{\"placements\":{\"widget-overflow-fixed-list\":[],\"unified-extensions-area\":[\"ublock0_raymondhill_net-browser-action\",\"_testpilot-containers-browser-action\",\"_d7742d87-e61d-4b78-b8a1-b469842139fa_-browser-action\",\"languagetool-webextension_languagetool_org-browser-action\",\"idcac-pub_guus_ninja-browser-action\",\"_8dd384e7-fc9e-4b6a-a744-497edc3408c3_-browser-action\",\"_c2c003ee-bd69-42a2-b0e9-6f34222cb046_-browser-action\"],\"nav-bar\":[\"back-button\",\"forward-button\",\"stop-reload-button\",\"personal-bookmarks\",\"vertical-spacer\",\"urlbar-container\",\"customizableui-special-spring2\",\"unified-extensions-button\"],\"toolbar-menubar\":[\"menubar-items\"],\"TabsToolbar\":[\"tabbrowser-tabs\"],\"vertical-tabs\":[],\"PersonalToolbar\":[],\"zen-sidebar-top-buttons\":[\"home-button\"],\"zen-sidebar-foot-buttons\":[\"downloads-button\",\"zen-workspaces-button\",\"zen-create-new-button\"]},\"seen\":[\"developer-button\",\"_d7742d87-e61d-4b78-b8a1-b469842139fa_-browser-action\",\"languagetool-webextension_languagetool_org-browser-action\",\"idcac-pub_guus_ninja-browser-action\",\"ublock0_raymondhill_net-browser-action\",\"_8dd384e7-fc9e-4b6a-a744-497edc3408c3_-browser-action\",\"_testpilot-containers-browser-action\",\"_c2c003ee-bd69-42a2-b0e9-6f34222cb046_-browser-action\",\"screenshot-button\"],\"dirtyAreaCache\":[\"nav-bar\",\"vertical-tabs\",\"zen-sidebar-foot-buttons\",\"PersonalToolbar\",\"toolbar-menubar\",\"TabsToolbar\",\"unified-extensions-area\",\"zen-sidebar-top-buttons\",\"widget-overflow-fixed-list\"],\"currentVersion\":22,\"newElementCount\":13}");
