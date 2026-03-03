1939426f6cfd47ee4d1db142085ca96433cfec50
Jakub Kadlcik <frostyx@email.cz>
2026-03-03 23:48:49 +0100

    nix, git: always pull with --rebase

--- .config/home-manager/git.nix
+++ .config/home-manager/git.nix
@@ -10,6 +10,9 @@
         name = "Jakub Kadlcik";
         email = "frostyx@email.cz";
       };
+      pull = {
+        rebase = true;
+      };
       alias = {
         lol = "log --graph --decorate --pretty=oneline --abbrev-commit";
         forcepull = "!git reset --hard main && git pull --rebase";
