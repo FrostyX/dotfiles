b34499c4d3d8de8be49d3301814b2f7ad3a6bea4
Jakub Kadlcik <frostyx@email.cz>
2026-09-13 14:20:02 +0200

    nix, zen: allow adblock in private windows

--- .config/home-manager/zen.nix
+++ .config/home-manager/zen.nix
@@ -76,6 +76,7 @@ lib.mkIf (builtins.elem hostname [ "pop-os" "nova" "hive" ]) {
         "uBlock0@raymondhill.net" = {
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
           installation_mode = "force_installed";
+          private_browsing = true;
         };
         "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
